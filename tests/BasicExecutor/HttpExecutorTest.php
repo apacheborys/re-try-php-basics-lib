052f3934ff01bc7f2b9da7bf93760094685b29ef
Borys Yermokhin <borys.ermokhin@gmail.com>
2022-12-02 18:10:30 +0100

    finish tests

--- tests/BasicExecutor/HttpExecutorTest.php
+++ tests/BasicExecutor/HttpExecutorTest.php
@@ -10,22 +10,25 @@ use PHPUnit\Framework\TestCase;
 
 class HttpExecutorTest extends TestCase
 {
+    private const HOST = 'http://localhost';
+
+    /** @dataProvider getDataSet */
     public function testHandling(string $method, array $expectedOptions): void
     {
-        $executor = new TestableHttpExecutor('http://localhost', 'POST');
+        $executor = new TestableHttpExecutor(self::HOST, $method);
 
         $message = new Message(
             '1',
             'retry-name',
             'correlation-id',
-            ['payload' => 'test'],
+            ['payload' => 'test', 'arguments' => ['variable' => 'value']],
             2,
             false,
             new \DateTimeImmutable(),
             HttpExecutor::class
         );
 
-        $this->assertSame($executor->getCurlOptionsForTest($message));
+        $this->assertSame($expectedOptions, $executor->getCurlOptionsForTest($message));
     }
 
     public function getDataSet(): iterable
@@ -34,11 +37,27 @@ class HttpExecutorTest extends TestCase
             'method' => HttpMethod::POST,
             'expectedOptions' => [
                 CURLOPT_USERAGENT => 'Retry PHP library. HttpExecutor',
-                10023 => 'RETRY_PHP_MESSAGE_ID: 1',
-                42 => true,
-                44 => true,
-                47 => 1,
-                10015 => '',
+                CURLOPT_HTTPHEADER => [
+                    'RETRY_PHP_MESSAGE_ID: 1',
+                ],
+                CURLOPT_HEADER => true,
+                CURLOPT_NOBODY => true,
+                CURLOPT_POST => 1,
+                CURLOPT_POSTFIELDS => 'variable=value',
+            ],
+        ];
+
+        yield [
+            'method' => HttpMethod::GET,
+            'expectedOptions' => [
+                CURLOPT_USERAGENT => 'Retry PHP library. HttpExecutor',
+                CURLOPT_HTTPHEADER => [
+                    'RETRY_PHP_MESSAGE_ID: 1',
+                ],
+                CURLOPT_HEADER => true,
+                CURLOPT_NOBODY => true,
+                CURLOPT_HTTPGET => true,
+                CURLOPT_URL => self::HOST . '?variable=value'
             ],
         ];
     }
